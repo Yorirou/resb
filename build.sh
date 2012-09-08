fe6f39ddd8871c74d9b49f81d6ec33d7f2fb7274
Tamás Demeter-Haludka <yorirou@gmail.com>
2012-09-08 21:36:05 +0200

    Turns off minification for js and adjusts the build script.

--- build.sh
+++ build.sh
@@ -9,16 +9,14 @@ git clone git@github.com:honestbleeps/Reddit-Enhancement-Suite.git tmp --depth 1
 BD="tmp/RES.safariextension"
 
 # Concatenate files together
-touch unprocessed.js
-cat $BD/jquery-1.6.4.min.js >> unprocessed.js
-cat $BD/guiders-1.2.8.js >> unprocessed.js
-cat $BD/jquery.dragsort-0.4.3.min.js >> unprocessed.js
-cat $BD/jquery-fieldselection.min.js >> unprocessed.js
-cat $BD/tinycon.min.js >> unprocessed.js
-cat $BD/jquery.tokeninput.js >> unprocessed.js
-cat $BD/snuownd.js >> unprocessed.js
-cat tmp/lib/reddit_enhancement_suite.user.js >> unprocessed.js
+touch bookmarklet.debug.js
+cat $BD/jquery-1.6.4.min.js >> bookmarklet.debug.js
+cat $BD/guiders-1.2.8.js >> bookmarklet.debug.js
+cat $BD/jquery.dragsort-0.4.3.min.js >> bookmarklet.debug.js
+cat $BD/jquery-fieldselection.min.js >> bookmarklet.debug.js
+cat $BD/tinycon.min.js >> bookmarklet.debug.js
+cat $BD/jquery.tokeninput.js >> bookmarklet.debug.js
+cat $BD/snuownd.js >> bookmarklet.debug.js
+cat tmp/lib/reddit_enhancement_suite.user.js >> bookmarklet.debug.js
 
 rm -rf tmp
-
-echo "Now pack the file with http://dean.edwards.name/packer/."
